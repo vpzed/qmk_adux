d540f2b830ec725970c5c00eb24babdd967d37db
vpzed <30138020+vpzed@users.noreply.github.com>
2022-05-01 17:33:43 -0500

    Adding additional config files

---
+++ config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#define TAPPING_TERM 200
+#define PERMISSIVE_HOLD
+#define IGNORE_MOD_TAP_INTERRUPT
+#define TAPPING_FORCE_HOLD
+
+#define COMBO_COUNT 4
---
+++ rules.mk
@@ -0,0 +1,2 @@
+COMBO_ENABLE = yes
+SRC += source.c
